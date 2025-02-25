f0251d8fde47fac7600f6f1d7b87b74e21e7f90a
Kibet Kipsang <denniskipsang2000@gmail.com>
2025-02-25 18:45:36 +0300

    testing

---
+++ lib/edu.py
@@ -0,0 +1,11 @@
+
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    def learn(self, new_knowledge):
+        if isinstance(new_knowledge, str) and new_knowledge.strip():
+            self.knowledge.append(new_knowledge)
+            return f"Learned: {new_knowledge}!"
+        else:
+            "No new knowledge!"
+        
